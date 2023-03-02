2e28b1c7a426d4ded304b08c04a433f7808f358e
SanjaySharma9945 <126223970+SanjaySharma9945@users.noreply.github.com>
2023-03-02 09:29:09 +0530

    first commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,9 +6,9 @@ function Home() {
     <div>
       <div className="banner-image"></div>
       <div>
-        <span className="tagline">JUST DOES IT</span>
+        <span className="tagline">JUST DO IT</span>
         <div className="tagline-para">
-          Nike has been your compadsdvsdvnion in happiness, both big and small, for
+          Nike has been your companion in happiness, both big and small, for
           over 50 years. Our roots are firmly grounded in India and with its
           people.
         </div>
